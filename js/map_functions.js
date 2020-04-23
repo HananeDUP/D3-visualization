17ff151726725ffa2d652a1ec3014559c334de88
HananeDUP <hanane.moualil@gmail.com>
2020-04-23 19:02:09 +0200

    Density and Population and filter done

--- js/map_functions.js
+++ js/map_functions.js
@@ -1,38 +1,34 @@
-let mapDrawPopDensity = function (w, g, selectedField, scaleMulti) {
-
+let mapDrawPopDensity = function (first_load, selectedField, scaleMulti, dataset, svg, cities, zoom, div, legendAxis) {
     if (selectedField == "population") {
         colorMap = "YlGnBu"
+
     } else if (selectedField == "density") {
         colorMap = "RdPu"
+        scaleMulti = 1
     }
+    svg.attr("class", colorMap)
+    console.log("after modif =", selectedField, colorMap, scaleMulti)
+    drawMap(selectedField, scaleMulti, dataset, svg, cities, zoom, div, legendAxis)
+}
 
-    //Create SVG element
-    let svg = d3.select("#mapDensityPop").append("svg")
-        .attr("width", w)
-        .attr("height", h)
-        .attr("class", colorMap); //Rouge Violet
+function drawMap(selectedField, scaleMulti, dataset, svg, cities, zoom, div, legendAxis) {
+    quantile = d3.scaleQuantile()
+        .domain([0, d3.mean(dataset, (row) => row[selectedField]) * scaleMulti])
+        .range(d3.range(9));
 
-    let cities = svg.append("g")
-        .attr('id', 'citiesmap')
-    // .attr('transform', 'translate(50, 50)')
-    // .attr("marginRight", 150);//.attr("width", wg - 200).attr("height", hg - 200);
+    legendScale = d3.scaleLinear()
+        // .domain([0, d3.median(rows, (row) => row.population) + 5000])
+        .domain([0, d3.mean(dataset, (row) => row[selectedField]) * scaleMulti])
+        .range([0, 9 * 20]);
+    if (first_load == 0) {
+        x = d3.scaleLinear()
+            .domain(d3.extent(dataset, (row) => row.longitude))
+            .range([0, w]);
 
-    var zoom = d3.zoom()
-        .on("zoom", function () {
-            svg.attr("transform", d3.event.transform)
-        })
+        y = d3.scaleLinear()
+            .domain(d3.extent(dataset, (row) => row.latitude))
+            .range([h, 0]);
 
-    var div = d3.select("#mapDensityPop").append("div")
-        .attr("class", "tooltip")
-        .style("opacity", 0);
-
-    var legend = svg.append('g')
-        .attr('transform', 'translate(520, 250)')
-        .attr('id', 'legend');
-
-    // let svgHisto = d3.select("body").append("svg").attr("width", histoWidth).attr("height", histoHeigth);
-
-    function drawMap() {
         cities.selectAll("rect")
             .data(dataset)
             .enter()
@@ -42,7 +38,7 @@ let mapDrawPopDensity = function (w, g, selectedField, scaleMulti) {
             .attr("x", (d) => x(d.longitude))
             .attr("y", (d) => y(d.latitude))
             // .attr("fill", (d) => myColor(d.population))
-            // .attr("id", (d) => "code_" + d.codePostal)
+            .attr("id", (d) => "code_" + d.codePostal)
             // .attr("class", d => "code_post q" + quantile(+d.population) + "-9")
             .attr("class", d => "code_post q" + quantile(+d[selectedField]) + "-9")
             .on("mouseover", function (d) {
@@ -67,79 +63,16 @@ let mapDrawPopDensity = function (w, g, selectedField, scaleMulti) {
                     .style("top", "-500px");
             });
 
-        legend.selectAll('.colorbar')
-            .data(d3.range(9))
-            .enter().append('svg:rect')
-            .attr('y', d => d * 20 + 'px')
-            .attr('height', '20px')
-            .attr('width', '20px')
-            .attr('x', '0px')
-            .attr("class", d => "q" + d + "-9")
-
-        svg.call(zoom)
-
-    };
-
-
-    d3.tsv("data/france.tsv")
-        .row((d, i) => {
-            return {
-                codePostal: +d["Postal Code"],
-                inseeCode: +d.inseecode,
-                place: d.place,
-                longitude: +d.x,
-                latitude: +d.y,
-                population: +d.population,
-                density: +d.density
-            };
+    } else {
+        console.log("in modified data")
+        dataset.forEach(function (e, i) {
+            console.log("#code_" + e.codePostal)
+            d3.select("#code_" + e.codePostal)
+                .attr("class", e => "code_post q" + quantile(+e[selectedField]) + "-9")
         })
-        .get((error, rows) => {
-            console.log("loaded " + rows.length + " rows");
-            if (rows.length > 0) {
-                console.log("First row: ", rows[0]);
-                console.log("Last row: ", rows[rows.length - 1]);
-                x = d3.scaleLinear()
-                    .domain(d3.extent(rows, (row) => row.longitude))
-                    // .domain([0, d3.max(rows, (row) => row.longitude)])
-                    .range([0, w]);
+    }
 
-                xinv = d3.scaleLinear()
-                    .domain(d3.extent(rows, (row) => row.longitude))
-                    // .domain([0, d3.max(rows, (row) => row.longitude)])
-                    .range([w, 0]);
+    legendAxis.call(d3.axisRight(legendScale).ticks(6));
 
-                y = d3.scaleLinear()
-                    .domain(d3.extent(rows, (row) => row.latitude))
-                    .range([h, 0]);
-
-
-                myColor = d3.scaleSequential()
-                    .domain([1, scalePop])
-                    .interpolator(d3.interpolatePuRd);
-
-                quantile = d3.scaleQuantile()
-                    // .domain([0, d3.median(rows, (row) => row.population) + 5000])
-                    .domain([0, d3.mean(rows, (row) => row[selectedField]) * scaleMulti])
-                    .range(d3.range(9));
-
-                var legendScale = d3.scaleLinear()
-                    // .domain(d3.extent(rows, (row) => row.population))
-                    // .domain([0, d3.median(rows, (row) => row.population) + 5000])
-                    .domain([0, d3.mean(rows, (row) => row[selectedField]) * scaleMulti])
-                    // .domain(d3.extent(dataset, (d) => d.population))
-                    .range([0, 9 * 20]);
-
-                console.log("domain min max", d3.median(rows, (row) => row.population), d3.mean(rows, (row) => row.population), d3.mean(rows, (row) => row.population) * scaleMulti)
-                console.log("domain min max", d3.median(rows, (row) => row[selectedField]), d3.mean(rows, (row) => row[selectedField]), d3.mean(rows, (row) => row[selectedField]) * scaleMulti)
-
-                dataset = rows;
-                drawMap()
-                svg.append("g")
-                    .attr('transform', 'translate(545, 250)')
-                    .call(d3.axisRight(legendScale).ticks(6));
-
-            };
-
-        });
-
-}
+    svg.call(zoom)
+};
